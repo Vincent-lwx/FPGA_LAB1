9584f23833dc622b64a91f8ac50de4c7c4dcbf76
Vincent-lwx <vincentliu0706@gmail.com>
2024-10-22 23:07:31 +0800

    Update com.v

--- com.v
+++ com.v
@@ -2,11 +2,13 @@ module com(
     input wire A,
     input wire B,
     output wire H,
-    output wire E
+    output wire E,
+	 output wire L
 );
 
-
-assign H =  B & ~A;
+assign H =  A & ~B;
 assign E =  A == B;
+assign L =  B & ~A;
+
 
 endmodule
